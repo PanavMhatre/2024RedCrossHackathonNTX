df870175c864ebaf67d7abaec98cb67415c95d10
rohansarakinti <sarakintirohan@gmail.com>
2024-01-21 12:54:41 -0600

    navbar

---
+++ redcrossapplication/app/home/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Navbar from './Navbar'
+export default function page() {
+  return (
+      <div className='w-screen h-screen bg-red-700'>
+        <Navbar />
+      </div>
+  )
+}
