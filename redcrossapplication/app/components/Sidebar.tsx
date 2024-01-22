9016a57e3e38b92c108c5b5ce524300b9b1e5107
rohansarakinti <sarakintirohan@gmail.com>
2024-01-21 23:38:27 -0600

    sidebar links update

--- redcrossapplication/app/components/Sidebar.tsx
+++ redcrossapplication/app/components/Sidebar.tsx
@@ -264,7 +264,7 @@ function Sidebar() {
 
               <a
                 className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
-                href="#"
+                href="https://www.usa.gov/"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -288,7 +288,7 @@ function Sidebar() {
 
               <a
                 className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
-                href="#"
+                href="https://www.redcross.org/"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
