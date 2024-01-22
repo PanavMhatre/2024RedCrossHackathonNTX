1d4f7254c67ba663685ad3fc55cab3ec9c255583
rohansarakinti <sarakintirohan@gmail.com>
2024-01-21 22:59:25 -0600

    home router fixes

--- redcrossapplication/app/home/Navbar.tsx
+++ redcrossapplication/app/home/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
         <header>
           <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
               <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                  <a href="/home" className="flex items-center">
+                  <a href="/" className="flex items-center">
                       <Image src={logo} className="mr-3 h-6 w-auto sm:h-9" alt="Red Cross Logo" />
                       <span className="self-center text-xl font-semibold whitespace-nowrap text-red-600 hover:text-red-500 duration-300">Red Alert</span>
                   </a>
@@ -22,7 +22,7 @@ export default function Navbar() {
                   <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                       <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                           <li>
-                              <a href="/home" className="block py-2 pr-4 pl-3 text-red-600 hover:text-red-500 duration-300rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 " aria-current="page">Home</a>
+                              <a href="/" className="block py-2 pr-4 pl-3 text-red-600 hover:text-red-500 duration-300rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 " aria-current="page">Home</a>
                           </li>
                           <li>
                               <a href="/about" className="block py-2 pr-4 pl-3 text-red-600 border-b hover:text-red-500 duration-300 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 ">About Us</a>
